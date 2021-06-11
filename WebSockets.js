f28cecbc1a93548c59c78e25e0e026df2246fa2e
Carlos lopez <carloslopezandres1997@gmail.com>
2021-06-11 15:52:49 -0500

    Update WebSockets.js

--- WebSockets.js
+++ WebSockets.js
@@ -1,21 +1,31 @@
 
-var socket = new WebSocket('ws://node-red-ugoae-2021-05-27.mybluemix.net/ws/audioInput');
+var socketRecord = new WebSocket('ws://node-red-ugoae-2021-05-27.mybluemix.net/ws/driverAudio');
 
-socket.onopen = function (event) {
+socketRecord.onopen = function (event) {
+    socketRecord.send("con");
     console.log('Conectado');
 };
-socket.onmessage = function (event) {
+socketRecord.onmessage = function (event) {
     var message = event.data;
-    console.log(message);
+    if(typeof(message)=="string"){
+        console.log(message);
+    }else{
+        console.log(message);
+    }
+    
 };
 
-socket.onclose = function (event) {
+socketRecord.onclose = function (event) {
     console.log('Desconectado del WebSocket.');
 };
 
 var constraints = { audio: true };
+
+var mediaRecorder;
+
 navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
-    var mediaRecorder = new MediaRecorder(mediaStream);
+
+    mediaRecorder = new MediaRecorder(mediaStream);
     mediaRecorder.onstart = function(e) {
         this.chunks = [];
     };
@@ -23,14 +33,15 @@ navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
         this.chunks.push(e.data);
     };
     mediaRecorder.onstop = function(e) {
-        var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
-        socket.send(blob);
+        var blob = new Blob(this.chunks, { 'type' : 'audio/mp3; codecs=opus' });
+        socketRecord.send(blob);
     };
+    
+});
 
+function grabar(){
     mediaRecorder.start();
-
     setTimeout(function() {
         mediaRecorder.stop()
     }, 2000);
-});
-
+}
